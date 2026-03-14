ff1784c23d585b8a0f67cd0f35a97bced501d409
Volpe08 <79302699+Volpe08@users.noreply.github.com>
2026-03-14 12:02:52 +0100

    Modification deploiement

--- prestashop-app/docker-entrypoint-custom.sh
+++ prestashop-app/docker-entrypoint-custom.sh
@@ -4,28 +4,14 @@ set -eu
 CUSTOM_MODULES_DIR="/opt/prestashop-custom-modules"
 TARGET_MODULES_DIR="/var/www/html/modules"
 
-CONFIG_FILE_1="/var/www/html/app/config/parameters.php"
-CONFIG_FILE_2="/var/www/html/config/settings.inc.php"
-
-echo "[init] Checking Prestashop installation state..."
-
-if [ -f "$CONFIG_FILE_1" ] || [ -f "$CONFIG_FILE_2" ]; then
-  echo "[init] Existing Prestashop config detected."
-  if [ -d /var/www/html/install ]; then
-    echo "[init] Removing install directory..."
-    rm -rf /var/www/html/install || true
-  fi
-else
-  echo "[init] No existing Prestashop config detected."
-fi
-
-echo "[init] Sync custom modules..."
+echo "[init] custom entrypoint started"
 
 if [ -d "$CUSTOM_MODULES_DIR" ]; then
+  echo "[init] syncing custom modules from image..."
   for module_path in "$CUSTOM_MODULES_DIR"/*; do
     if [ -d "$module_path" ]; then
       module_name="$(basename "$module_path")"
-      echo "[init] Installing/updating custom module: $module_name"
+      echo "[init] sync module: $module_name"
       rm -rf "$TARGET_MODULES_DIR/$module_name"
       mkdir -p "$TARGET_MODULES_DIR/$module_name"
       cp -a "$module_path"/. "$TARGET_MODULES_DIR/$module_name"/
@@ -33,5 +19,4 @@ if [ -d "$CUSTOM_MODULES_DIR" ]; then
   done
 fi
 
-echo "[init] Starting original Prestashop startup..."
 exec "$@"
